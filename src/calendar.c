a4f34016243a8d4f5450308a978deffe867936d4
Removeris <108592864+removeris@users.noreply.github.com>
2026-06-07 01:54:40 +0300

    formatting fix

--- src/calendar.c
+++ src/calendar.c
@@ -8,16 +8,16 @@ int main(void) {
 	time_t raw_time;
 	struct tm *time_info;
 
-        raw_time = time(NULL);
+	raw_time = time(NULL);
 	time_info = localtime(&raw_time);
 	
-        enum WEEK_DAY first_day;
-        first_day = find_first_week_day(time_info->tm_mday,
+	enum WEEK_DAY first_day;
+	first_day = find_first_week_day(time_info->tm_mday,
 					time_info->tm_wday);
 
         int total_days = find_days_in_month(time_info->tm_mon,
-                                            time_info->tm_year);
-
+					    time_info->tm_year);
+	
         print_calendar(total_days, first_day, time_info->tm_mday);
 	
         return 0;
